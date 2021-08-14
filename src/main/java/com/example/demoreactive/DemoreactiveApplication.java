12ada7e88ad530ed42219e37a9f935a775d94dcd
T4puSD <sdtapusd20@gmail.com>
2021-08-14 23:45:27 +0600

    removed application runner

--- src/main/java/com/example/demoreactive/DemoreactiveApplication.java
+++ src/main/java/com/example/demoreactive/DemoreactiveApplication.java
@@ -1,31 +1,12 @@
 package com.example.demoreactive;
 
-import com.example.demoreactive.domain.Event;
-import com.example.demoreactive.domain.enumuration.EventName;
-import com.example.demoreactive.repository.EventRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoreactiveApplication implements ApplicationRunner{
+public class DemoreactiveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoreactiveApplication.class, args);
-	}
-
-	@Autowired
-	EventRepository eventRepository;
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		System.out.println("RUnning");
-		eventRepository.save(new Event(EventName.QUOTE, "This is a first event"))
-				.subscribe(event -> {
-					System.out.println("Id is:"+ event.getId());
-				});
-		System.out.println("Ran");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoreactiveApplication.class, args);
+    }
 }
